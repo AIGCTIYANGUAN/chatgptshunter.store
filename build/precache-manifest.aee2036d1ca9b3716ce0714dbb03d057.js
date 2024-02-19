29693c56a60606e6e3a3bb600428c3cda03bcf1e
AIGCTIYANGUAN <kejinyang@icloud.com>
2024-02-19 15:49:31 +0800

    项目初始化

---
+++ build/precache-manifest.aee2036d1ca9b3716ce0714dbb03d057.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2575f3521db207fabc626d287e8ea6d7",
+    "url": "/chatgptshunter.store/index.html"
+  },
+  {
+    "revision": "96e662d8dfcec3759e8c",
+    "url": "/chatgptshunter.store/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "1f5929bab9e74fc2d234",
+    "url": "/chatgptshunter.store/static/js/2.a71b1702.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/chatgptshunter.store/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "96e662d8dfcec3759e8c",
+    "url": "/chatgptshunter.store/static/js/main.01b01939.chunk.js"
+  },
+  {
+    "revision": "c71307a50cf58609a26a",
+    "url": "/chatgptshunter.store/static/js/runtime~main.28528eb4.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/chatgptshunter.store/static/media/logo.06e73328.svg"
+  }
+]);
